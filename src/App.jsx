086e234e645d9051d21a52cab27158c26acb7877
Marcelo Lamaruggine <marcelolama.27@gmail.com>
2024-01-09 13:54:02 -0300

    iniciamos proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/layout/footer/Footer";
+import Navbar from "./components/layout/navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
