b8c940111f02542f746e6cc39c3b2ceebe4d7540
Lamaruggine <MLAMARUG@tmoviles.com.ar>
2024-01-11 18:48:00 -0300

    agregamos barra de navegacion y logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Footer from "./components/layout/footer/Footer";
 import Navbar from "./components/layout/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Footer />
     </>
   );
 }
--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,50 +1,44 @@
 import React from "react";
+import PhoneIcon from '@mui/icons-material/Phone';
+import "../navbar/Navbar.css"
 
 const Navbar = () => {
   return (
-    <div style={{ marginTop: 30 }}>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-          </div>
+    <div>
+      <div>
+        <div style={{display:"flex", alignItems: "center", opacity: 0.7, height:60}}>
+          <div> <PhoneIcon /></div>
+          <div><h4>1122334455</h4></div>
         </div>
-      </nav>
+      </div>
+      <div>
+        <div>
+          <nav className="navbar navbar-expand-lg myClass">
+            <div className="container-fluid ">
+              <img style={{width:90, height:80}} src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705007748/veterinaryProject/Logo1_qrp9ji.png" alt="" />
+              <div className="collapse navbar-collapse" id="navbarNav"  >
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Features
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Pricing
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 };
