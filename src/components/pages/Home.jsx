8661a6bd0326944d004441fda14947bbf44f60c4
Lamaruggine <MLAMARUG@tmoviles.com.ar>
2024-01-15 16:32:48 -0300

    agregamos transicion lenta de los textos e imagenes

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -41,49 +41,81 @@ const Home = () => {
       </div>
       <div>
         <div className='divTituloServicio' style={{marginTop:80}}><h2 className='tituloServicio'>Servicios</h2></div>
-        <div class="container text-center" style={{marginTop:50}}>
-          <div class="row row-cols-3">
-            <div class="col">
+        <div className="container text-center" style={{marginTop:50}}>
+          <div className="row row-cols-3">
+            <div className="col">
+              <div>
               <img className='imgServicio' src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705284067/veterinaryProject/Sin-t%C3%ADtulo-1_ypstvn.png" alt="" />
+              </div>
               <p className='nombreServicio'>Clínica</p>
               <div className='contenedorDescripcion'>
-                <p className='descripcionServicio'>Clínica Veterinaria Amigos de las Mascotas: Un hogar lejos del hogar para tus amigos de cuatro patas. Nuestro equipo de expertos veterinarios se dedica a proporcionar atención médica de alta calidad, desde chequeos rutinarios hasta cirugías especializadas. Con servicios que incluyen diagnósticos, tratamientos, vacunaciones, y cuidado preventivo, nos esforzamos por mantener a tus mascotas saludables y felices. ¡Ven y experimenta el cuidado compasivo que tus mascotas se merecen!</p>
+                <p className='descripcionServicio'>Nuestra <strong>clínica</strong> veterinaria ofrece una amplia gama de servicios para mantener a tus amigos peludos en la mejor salud posible. Contamos con un equipo de veterinarios altamente capacitados y amantes de los animales, dedicados a brindar el más alto nivel de atención. Ofrecemos servicios de diagnóstico avanzados, tratamientos médicos y quirúrgicos, vacunaciones, y un servicio de urgencias disponible las 24 horas. Además, proporcionamos asesoramiento nutricional y comportamental para ayudarte a entender y cuidar mejor a tus mascotas.</p>
               </div>
             </div>
-            <div class="col">
+            <div className="col">
               <img className='imgServicio' src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705284067/veterinaryProject/Sin-t%C3%ADtulo-1_ypstvn.png" alt="" />
               <p className='nombreServicio'>Cirugía General</p>
               <div className='contenedorDescripcion'>
-                <p className='descripcionServicio'>Clínica Veterinaria Amigos de las Mascotas: Un hogar lejos del hogar para tus amigos de cuatro patas. Nuestro equipo de expertos veterinarios se dedica a proporcionar atención médica de alta calidad, desde chequeos rutinarios hasta cirugías especializadas. Con servicios que incluyen diagnósticos, tratamientos, vacunaciones, y cuidado preventivo, nos esforzamos por mantener a tus mascotas saludables y felices. ¡Ven y experimenta el cuidado compasivo que tus mascotas se merecen!</p>
+                <p className='descripcionServicio'>Nuestro equipo de <strong>cirugía general</strong> está dedicado a proporcionar atención quirúrgica de alta calidad y personalizada. Contamos con cirujanos altamente capacitados y experimentados, comprometidos con la excelencia en el cuidado del paciente. Ofrecemos una amplia gama de procedimientos quirúrgicos, desde cirugías mínimamente invasivas hasta operaciones complejas. Nuestro objetivo es asegurar que cada paciente reciba el tratamiento más adecuado y efectivo para su condición específica.</p>
               </div>
             </div>
-            <div class="col">
+            <div className="col">
               <img className='imgServicio' src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705284067/veterinaryProject/Sin-t%C3%ADtulo-1_ypstvn.png" alt="" />
               <p className='nombreServicio'>Ecografía</p>
               <div className='contenedorDescripcion'>
-                <p className='descripcionServicio'>Clínica Veterinaria Amigos de las Mascotas: Un hogar lejos del hogar para tus amigos de cuatro patas. Nuestro equipo de expertos veterinarios se dedica a proporcionar atención médica de alta calidad, desde chequeos rutinarios hasta cirugías especializadas. Con servicios que incluyen diagnósticos, tratamientos, vacunaciones, y cuidado preventivo, nos esforzamos por mantener a tus mascotas saludables y felices. ¡Ven y experimenta el cuidado compasivo que tus mascotas se merecen!</p>
+                <p className='descripcionServicio'>Nuestro servicio de <strong>ecografía</strong> está comprometido con la excelencia en el diagnóstico por imagen. Contamos con un equipo de profesionales altamente capacitados y tecnología de última generación para proporcionar imágenes claras y precisas. Ofrecemos una amplia gama de exámenes de ecografía, incluyendo ecografías abdominales, obstétricas, musculoesqueléticas, entre otras. Nuestro objetivo es proporcionar un diagnóstico preciso que ayude a guiar el tratamiento y el cuidado del paciente.</p>
               </div>
             </div>
-            <div class="col">
+            <div className="col">
               <img className='imgServicio' src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705284067/veterinaryProject/Sin-t%C3%ADtulo-1_ypstvn.png" alt="" />
               <p className='nombreServicio'>Radiología</p>
               <div className='contenedorDescripcion'>
-                <p className='descripcionServicio'>Clínica Veterinaria Amigos de las Mascotas: Un hogar lejos del hogar para tus amigos de cuatro patas. Nuestro equipo de expertos veterinarios se dedica a proporcionar atención médica de alta calidad, desde chequeos rutinarios hasta cirugías especializadas. Con servicios que incluyen diagnósticos, tratamientos, vacunaciones, y cuidado preventivo, nos esforzamos por mantener a tus mascotas saludables y felices. ¡Ven y experimenta el cuidado compasivo que tus mascotas se merecen!</p>
+                <p className='descripcionServicio'>Nuestro servicio de <strong>radiología</strong> se dedica a proporcionar diagnósticos precisos y seguros a través de la imagenología. Contamos con un equipo de radiólogos altamente capacitados y tecnología avanzada para asegurar la calidad y precisión de nuestras imágenes. Ofrecemos una variedad de exámenes de radiología, incluyendo rayos X, tomografías computarizadas (TC), resonancias magnéticas (RM), entre otros. Nuestro objetivo es proporcionar un diagnóstico claro y preciso que permita a tu médico determinar el mejor plan de tratamiento para ti.</p>
               </div>
             </div>
-            <div class="col">
+            <div className="col">
               <img className='imgServicio' src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705284067/veterinaryProject/Sin-t%C3%ADtulo-1_ypstvn.png" alt="" />
               <p className='nombreServicio'>Internación</p>
               <div className='contenedorDescripcion'>
-                <p className='descripcionServicio'>Clínica Veterinaria Amigos de las Mascotas: Un hogar lejos del hogar para tus amigos de cuatro patas. Nuestro equipo de expertos veterinarios se dedica a proporcionar atención médica de alta calidad, desde chequeos rutinarios hasta cirugías especializadas. Con servicios que incluyen diagnósticos, tratamientos, vacunaciones, y cuidado preventivo, nos esforzamos por mantener a tus mascotas saludables y felices. ¡Ven y experimenta el cuidado compasivo que tus mascotas se merecen!</p>
+                <p className='descripcionServicio'>Nuestro servicio de <strong>internación</strong> está dedicado a proporcionar atención médica integral y personalizada. Contamos con un equipo de profesionales de la salud altamente capacitados y comprometidos con el bienestar de nuestros pacientes. Ofrecemos habitaciones cómodas y bien equipadas para asegurar que tu estancia sea lo más agradable posible. Nuestro personal de enfermería está disponible las 24 horas del día para atender tus necesidades y asegurar tu comodidad.</p>
               </div>
             </div>
-            <div class="col">
+            <div className="col">
               <img className='imgServicio' src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705284067/veterinaryProject/Sin-t%C3%ADtulo-1_ypstvn.png" alt="" />
               <p className='nombreServicio'>Laboratorio</p>
               <div className='contenedorDescripcion'>
-                <p className='descripcionServicio'>Clínica Veterinaria Amigos de las Mascotas: Un hogar lejos del hogar para tus amigos de cuatro patas. Nuestro equipo de expertos veterinarios se dedica a proporcionar atención médica de alta calidad, desde chequeos rutinarios hasta cirugías especializadas. Con servicios que incluyen diagnósticos, tratamientos, vacunaciones, y cuidado preventivo, nos esforzamos por mantener a tus mascotas saludables y felices. ¡Ven y experimenta el cuidado compasivo que tus mascotas se merecen!</p>
+                <p className='descripcionServicio'>Nuestro servicio de <strong>laboratorio</strong> está comprometido con proporcionar resultados precisos y confiables. Contamos con un equipo de profesionales altamente capacitados y tecnología de vanguardia para asegurar la calidad de nuestros análisis. Ofrecemos una amplia gama de pruebas de laboratorio, incluyendo análisis de sangre, pruebas de orina, biopsias, entre otros. Nuestro objetivo es proporcionar resultados rápidos y precisos que ayuden a tu médico a determinar el mejor plan de tratamiento para ti.</p>
               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style={{marginTop: 100}}>
+        <div className="container text-center">
+          <div className="row row-cols-4">
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345996/veterinaryProject/8_ged25s.jpg" alt="" />
+            </div>
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345996/veterinaryProject/7_lsfefc.jpg" alt="" />
+            </div>
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345996/veterinaryProject/5_qqfck8.jpg" alt="" />
+            </div>
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345996/veterinaryProject/6_boi2fb.jpg" alt="" />
+            </div>
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345996/veterinaryProject/6_boi2fb.jpg" alt="" />
+            </div>
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345995/veterinaryProject/1_evlqvw.jpg" alt="" />
+            </div>
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345995/veterinaryProject/3_w1r0xh.jpg" alt="" />
+            </div>
+            <div className="col">
+              <img className="imagenesMuestra" src="https://res.cloudinary.com/dhdhzwlz9/image/upload/v1705345995/veterinaryProject/2_yssoao.jpg" alt="" />
             </div>
           </div>
         </div>
